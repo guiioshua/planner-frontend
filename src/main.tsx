81e4562d3ad4df673d04d0f70b1ceba97def146b
guiioshua <guilhermeioshua@gmail.com>
2026-02-18 12:06:04 -0300

    fix: bug the router incorreto no entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./index.css";
-import { AppProvider } from "./context/AppContext";
-
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
